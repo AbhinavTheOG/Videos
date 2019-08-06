fcb379748fd22ec5113b71a99937beaa00ba7780
AbhinavTheOG <48139542+AbhinavTheOG@users.noreply.github.com>
2019-08-06 10:56:31 -0700

    Implemented to Video detail component
    Now able to display the video after selecting a video from the video list

--- videos/src/Components/App.js
+++ videos/src/Components/App.js
@@ -1,19 +1,32 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
-
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 class App extends React.Component {
-  onTermSubmit = term => {
-    youtube.get("search", {
+  state = { videos: [], selectedVideo: null };
+  onTermSubmit = async term => {
+    const response = await youtube.get("search", {
       params: {
         q: term
       }
     });
+
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
